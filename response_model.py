583967ed662edb045baa5742813a6dc4ff85324f
vishwageethanjana <geeth.me67@gmail.com>
2026-03-05 20:47:44 +0530

    add response models

---
+++ response_model.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class TeacherProfileBase(BaseModel):
+    qualifications: Optional[str] = Field(None , max_length=300)
+    department: Optional[str] = Field(None , max_length=300)
+    office_number: Optional[str] = Field(None , max_length=30)
+    bio: Optional[str] = None
+
+class TeacherProfileCreate(TeacherProfileBase):
+    pass
+
+class TeacherProfileUpdate(TeacherProfileBase):
+    pass
+
+class TeacherProfileResponse(TeacherProfileBase):
+    id: int
+    teacher_id: int
+
+    class Config:
+        from_attributes = True
+
+
+#------------------------------------------------------------------------------#
+
+
+class TeacherBase(BaseModel):
+    name: str = Field(..., min_length=1, max_length=100)
+    email: str
+
+class TeacherCreate(TeacherBase):
+    profile: Optional[TeacherProfileCreate] = None
+
+class TeacherResponse(TeacherBase):
+    id: int
+    created_at: datetime
+    profile: Optional[TeacherProfileResponse] = None
+
+    class Config:
+        from_attributes = True
