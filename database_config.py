ae109bb0430732aa2a0ec3caedd146d3e7fe24c5
vishwageethanjana <geeth.me67@gmail.com>
2026-03-05 20:44:12 +0530

    add database config

---
+++ database_config.py
@@ -0,0 +1,20 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+
+
+DATABASE_URL = "postgresql+asyncpg://cclarke:admin@localhost:5432/institute_db"
+
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,         #set to true if you want to see sql statements produced by orm
+    pool_pre_ping=True,     #verify the connections in pool before using then
+    future=True,
+)
+
+async_session = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autocommit=False,
+)
